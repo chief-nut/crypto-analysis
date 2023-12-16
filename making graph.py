db3e1217b4e8f16e251097b558b04d84edf2a737
chief-nut <anchalyeaswaran2000@gmail.com>
2023-12-16 19:32:06 +0000

    hi

---
+++ making graph.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+import pandas as pd
+from matplotlib import pyplot as plt
+from datetime import date, timedelta
+
+BTC = yf.Ticker('BTC')
+BTC_Data = BTC.history(period="max")
+BTC_Data.head()
