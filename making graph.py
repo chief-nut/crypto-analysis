dc117fe4641a0fe1e666116631d408e1d75f7028
chief-nut <anchalyeaswaran2000@gmail.com>
2023-12-16 20:56:01 +0000

    commit 2

--- making graph.py
+++ making graph.py
@@ -1,10 +1,14 @@
 import yfinance as yf
 import pandas as pd
 from matplotlib import pyplot as plt
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
-BTC = yf.Ticker('BTC')
-BTC_Data = BTC.history(period="max")
-BTC_Data.head()
-print(BTC_Data)
+def get_data(name):                                     #get coin data
+    end_date = datetime.now().strftime('%Y-%m-%d')      #get todays date
+    BTC = yf.Ticker(name)                               
+    Coin_Data = BTC.history(period="max")               #get history of coin
+    Coin_Data.head()                                    #get first 5 rows of dataframe
+    print(Coin_Data)
 
+get_data('BTC-GBP')                                     #get history of btc
+get_data('ETH-GBP')
