16cad089fbe40ffabb5562f1e8db7f629502f8fc
chief-nut <anchalyeaswaran2000@gmail.com>
2023-12-17 14:48:37 +0000

    added simple graph

--- making graph.py
+++ making graph.py
@@ -2,13 +2,29 @@ import yfinance as yf
 import pandas as pd
 from matplotlib import pyplot as plt
 from datetime import date, timedelta, datetime
+import plotly.graph_objects as go
 
-def get_data(name):                                     #get coin data
-    end_date = datetime.now().strftime('%Y-%m-%d')      #get todays date
+end_date = datetime.now().strftime('%Y-%m-%d')
+
+def get_data(name):                                     #get coin data    
     BTC = yf.Ticker(name)                               
-    Coin_Data = BTC.history(period="max")               #get history of coin
-    Coin_Data.head()                                    #get first 5 rows of dataframe
-    print(Coin_Data)
+    Coin_Data = BTC.history(period="max")               #get history of coin                                  #get first 5 rows of dataframe
+    return Coin_Data
 
-get_data('BTC-GBP')                                     #get history of btc
-get_data('ETH-GBP')
+def print_data(name):
+    coin_data_DF = get_data(name)                       #set old func to variable
+    open = coin_data_DF['Open']                         #separating by column                       
+    high = coin_data_DF['High']
+    low = coin_data_DF['Low']
+    close = coin_data_DF['Close']
+    return open,high,low,close
+
+def plot_data(name):
+    OHLC_data = print_data(name)
+    open = OHLC_data[0]
+    close = OHLC_data[1]
+    open.plot()
+    close.plot()
+    plt.show()
+
+plot_data('BTC-GBP')
