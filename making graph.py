abb6a194c034c0d591148750129158d4f007d64a
chief-nut <anchalyeaswaran2000@gmail.com>
2023-12-17 16:37:00 +0000

    added title to graph

--- making graph.py
+++ making graph.py
@@ -13,15 +13,15 @@ def get_data(name):                                     #get coin data
 
 def plot_data(name):
     coin_data_DF = get_data(name)                       #set old func to variable
-    open = coin_data_DF['Open']                 #separating by column and plot                      
+    open = coin_data_DF['Open']                         #separating by column and plot                      
     high = coin_data_DF['High']
     low = coin_data_DF['Low']
     close = coin_data_DF['Close']
     date = coin_data_DF.index
     
-    fig = go.Figure(data=go.Candlestick(x=date,open=open,high=high,low=low,close=close))            #create graph
+    fig = go.Figure(data=go.Candlestick(x=date,open=open,high=high,low=low,close=close))                     #create graph
+    fig.update_layout(title = name + ' history', title_font_size = 50)
+
     fig.show()
 
-
-
-plot_data('BTC-GBP')
+plot_data('META')
